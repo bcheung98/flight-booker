c3444fa647e38753200877ae1df17981680c8a2e
Brian <briantcheung@gmail.com>
2021-05-06 16:04:39 -0700

    Created basic page setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home";
+
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from "react-router-dom";
+
+
+class App extends React.Component {
+
+    state = {
+        logged_in: false,
+        token: null
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+
+                        <Route exact path="/" component={Home} />
+
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
+
 }
 
 export default App;
