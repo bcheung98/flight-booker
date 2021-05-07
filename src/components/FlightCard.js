86d9a02b0281d49034ee1f8efdcac819ab0bff18
Brian <briantcheung@gmail.com>
2021-05-07 15:30:19 -0700

    Added confirmation popup to flight browser

--- src/components/FlightCard.js
+++ src/components/FlightCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router";
+import Popup from "reactjs-popup";
 
 import "../css/FlightCard.css";
 
@@ -9,6 +10,33 @@ const FlightCard = (props) => {
         <div className="flight-card">
             <p>{airline} Flight {flight_number}</p>
             <p>{origin} ==={">"} {destination}</p>
+            <Popup
+                trigger={<button className="add-flight-button">Book Flight</button>}
+                modal
+                nested
+            >
+                {close => (
+                    <div className="modal">
+                        <button className="modal-close" onClick={close}>
+                            &times;
+                        </button>
+                        <div className="modal-header"> Confirm Booking </div>
+                        <div className="modal-content">
+                            {' '}
+                            {airline} Flight {flight_number}
+                            <br />
+                            {origin} ==={">"} {destination}
+                        </div>
+                        <div className="modal-actions">
+                            <button className="modal-button" onClick={() => {
+                                console.log("Flight booked!")
+                                close();
+                            }}>Book Flight</button>
+                            <button className="modal-button" onClick={() => close()}>Cancel</button>
+                        </div>
+                    </div>
+                )}
+            </Popup>
         </div>
     )
 }
