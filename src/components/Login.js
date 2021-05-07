543231911d341656235a32d47926566ba2474c7f
Brian <briantcheung@gmail.com>
2021-05-06 17:31:41 -0700

    Added login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,35 @@
 import React from "react";
+import "../css/Login.css";
 import { withRouter } from "react-router";
 
 class Login extends React.Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        console.log("test");
+    }
+
     render() {
         return (
-            <div></div>
+            <div className="login-page">
+                <img src={require("../assets/plane1.jpg").default} alt="plane" className="background-img" />
+                <form onSubmit={this.handleSubmit} className="login-container">
+                    Username: <input type="text" className="form-input" />
+                    <br />
+                    Password: <input type="password" className="form-input" />
+                    <button className="form-input-button" type="submit">Login</button> 
+                </form>
+            </div>
         )
     }
 
